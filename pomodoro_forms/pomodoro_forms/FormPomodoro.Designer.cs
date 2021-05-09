a599c47a4b6b3d92a774990e380fdb6230c1f3d5
Craig Irving <craignirving@outlook.com>
2021-05-09 02:46:12 +0100

    added missing files

--- pomodoro_forms/pomodoro_forms/FormPomodoro.Designer.cs
+++ pomodoro_forms/pomodoro_forms/FormPomodoro.Designer.cs
@@ -70,6 +70,7 @@ namespace pomodoro_forms
             this.lblDfActivity = new System.Windows.Forms.Label();
             this.lblDfActivityColon = new System.Windows.Forms.Label();
             this.lblDfRestColon = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
             this.tabTimer.SuspendLayout();
             this.tabMain.SuspendLayout();
             this.tabDefaults.SuspendLayout();
@@ -334,6 +335,7 @@ namespace pomodoro_forms
             // 
             // tabDefaults
             // 
+            this.tabDefaults.Controls.Add(this.btnSave);
             this.tabDefaults.Controls.Add(this.btnSet);
             this.tabDefaults.Controls.Add(this.lblDfLongRest);
             this.tabDefaults.Controls.Add(this.lblDfLongRestColon);
@@ -362,7 +364,7 @@ namespace pomodoro_forms
             this.btnSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSet.Location = new System.Drawing.Point(248, 49);
             this.btnSet.Name = "btnSet";
-            this.btnSet.Size = new System.Drawing.Size(107, 187);
+            this.btnSet.Size = new System.Drawing.Size(107, 130);
             this.btnSet.TabIndex = 41;
             this.btnSet.Text = "Set";
             this.btnSet.UseVisualStyleBackColor = true;
@@ -508,6 +510,16 @@ namespace pomodoro_forms
             this.lblDfRestColon.TabIndex = 29;
             this.lblDfRestColon.Text = ":";
             // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(248, 205);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(107, 35);
+            this.btnSave.TabIndex = 42;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
             // FormPomodoro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -568,6 +580,7 @@ namespace pomodoro_forms
         private System.Windows.Forms.Label lblDfActivityColon;
         private System.Windows.Forms.Label lblDfRestColon;
         private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
